156f19a08d0a0de51adcb134064b75121a60fd6d
Stefan Langer <langer@docear.org>
2013-03-22 10:03:05 +0100

    - do not normalize titles -> they are normalized by the webservice

--- docear_plugin_services/src/org/docear/plugin/services/recommendations/RecommendationEntry.java
+++ docear_plugin_services/src/org/docear/plugin/services/recommendations/RecommendationEntry.java
@@ -14,22 +14,10 @@ public class RecommendationEntry {
 	
 	public RecommendationEntry(String prefix, String title, String url, String clickUrl, boolean highlighted) throws MalformedURLException {
 		this.prefix = prefix;
-		this.title = normalize(title);
+		this.title = title;
 		this.link = (url==null ? null:new URL(url));
 		this.clickUrl = (clickUrl==null ? null:new URL(clickUrl));
 		this.highlighted = highlighted;
-	}
-
-	private String normalize(String str) {
-		String[] tokens = str.split("\\s+");
-		StringBuilder sb = new StringBuilder();
-		for (String token : tokens) {
-			String tmp = token.trim();
-			sb.append(tmp.charAt(0));
-			sb.append(tmp.substring(1).toLowerCase());
-			sb.append(" ");
-		}
-		return sb.toString().trim();
 	}
 	
 	public String getPrefix() {
