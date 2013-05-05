9e9f5bb70481b7c11a3983dde2384112f0614b9d
AlexSchwank <alschwank@gmail.com>
2013-05-05 16:39:48 +0200

    added missing imports

--- freeplane_plugin_remote_server/src/org/freeplane/plugin/remote/server/Activator.java
+++ freeplane_plugin_remote_server/src/org/freeplane/plugin/remote/server/Activator.java
@@ -1,7 +1,9 @@
 package org.freeplane.plugin.remote.server;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
