89c75f5dd305d7d1489d51ab403f3a72d0f90b89
AlexSchwank <alschwank@gmail.com>
2013-06-10 17:06:32 +0200

    added config file for logger.

--- freeplane_framework/script/freeplane-server.sh
+++ freeplane_framework/script/freeplane-server.sh
@@ -201,6 +201,7 @@ _debug "Calling: "\
  "-Dorg.knopflerfish.framework.bundlestorage=memory"\
  "-Dorg.freeplane.globalresourcedir=${freedir}/resources"\
  "-Dorg.knopflerfish.gosg.jars=reference:file:${freedir}/core/"\
+ "-Dlogback.configurationFile=${freedir}/ci-logger.xml"\
  "-Dorg.freeplane.main.application.FreeplaneStarter.headless=true"\
  $defines\
  $xdockname\
@@ -220,6 +221,7 @@ _debug "Calling: "\
  "-Dorg.knopflerfish.framework.bundlestorage=memory"\
  "-Dorg.freeplane.globalresourcedir=${freedir}/resources"\
  "-Dorg.knopflerfish.gosg.jars=reference:file:${freedir}/core/"\
+ "-Dlogback.configurationFile=${freedir}/ci-logger.xml"\
  "-Dorg.freeplane.main.application.FreeplaneStarter.headless=true"\
  $defines\
  $xdockname\
