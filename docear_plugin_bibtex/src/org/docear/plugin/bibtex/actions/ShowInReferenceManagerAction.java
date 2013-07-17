55a1d31070882662293cee133f0b9c043439cb37
Stefan Langer <langer@docear.org>
2013-07-17 14:08:55 +0200

    change to BibTeX file according to the map's project

--- docear_plugin_bibtex/src/org/docear/plugin/bibtex/actions/ShowInReferenceManagerAction.java
+++ docear_plugin_bibtex/src/org/docear/plugin/bibtex/actions/ShowInReferenceManagerAction.java
@@ -2,12 +2,16 @@ package org.docear.plugin.bibtex.actions;
 
 import java.awt.event.ActionEvent;
 
+import org.docear.plugin.bibtex.JabRefProjectExtension;
 import org.docear.plugin.bibtex.ReferencesController;
 import org.docear.plugin.bibtex.jabref.JabRefCommons;
 import org.freeplane.core.ui.AFreeplaneAction;
 import org.freeplane.core.ui.EnabledAction;
 import org.freeplane.features.map.NodeModel;
 import org.freeplane.features.mode.Controller;
+import org.freeplane.plugin.workspace.WorkspaceController;
+import org.freeplane.plugin.workspace.features.WorkspaceMapModelExtension;
+import org.freeplane.plugin.workspace.model.project.AWorkspaceProject;
 
 @EnabledAction(checkOnNodeChange=true)
 public class ShowInReferenceManagerAction extends AFreeplaneAction {
@@ -42,6 +46,12 @@ public class ShowInReferenceManagerAction extends AFreeplaneAction {
 		NodeModel node = Controller.getCurrentModeController().getMapController().getSelectedNode();
 		if (node == null) {
 			return;
+		}
+		WorkspaceMapModelExtension modelExt = WorkspaceController.getMapModelExtension(node.getMap(), false);
+		if(modelExt != null) {
+			AWorkspaceProject project = modelExt.getProject();
+			JabRefProjectExtension ext = (JabRefProjectExtension) project.getExtensions(JabRefProjectExtension.class);
+			ext.selectBasePanel();
 		}
 		if(ReferencesController.getController().getJabrefWrapper().getBasePanel().getSelectedEntries().length <= 1) {
 			final String bibtexKey = ReferencesController.getController().getJabRefAttributes().getBibtexKey(node);			
