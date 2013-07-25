20c1e522bc3aa4ead1da49b56e71babcad86c42f
Stefan Langer <langer@docear.org>
2013-07-25 11:15:35 +0200

    remodeled view task (part of #24)
    - removed some actions from zoom band (already used in another band)
    - moved some actions to view mode band
    - added accelerator and tooltip to hide ribbon action

--- freeplane/src/org/freeplane/core/ui/ribbon/special/ZoomContributorFactory.java
+++ freeplane/src/org/freeplane/core/ui/ribbon/special/ZoomContributorFactory.java
@@ -6,9 +6,7 @@ import java.util.List;
 import java.util.Properties;
 
 import javax.swing.JComboBox;
-import javax.swing.KeyStroke;
 
-import org.freeplane.core.resources.SetBooleanPropertyAction;
 import org.freeplane.core.ui.AFreeplaneAction;
 import org.freeplane.core.ui.ribbon.ARibbonContributor;
 import org.freeplane.core.ui.ribbon.CurrentState;
@@ -25,11 +23,6 @@ import org.freeplane.view.swing.map.MapViewController;
 import org.pushingpixels.flamingo.api.common.AbstractCommandButton;
 import org.pushingpixels.flamingo.api.common.CommandButtonDisplayState;
 import org.pushingpixels.flamingo.api.common.JCommandButton;
-import org.pushingpixels.flamingo.api.common.JCommandButton.CommandButtonKind;
-import org.pushingpixels.flamingo.api.common.JCommandToggleMenuButton;
-import org.pushingpixels.flamingo.api.common.popup.JCommandPopupMenu;
-import org.pushingpixels.flamingo.api.common.popup.JPopupPanel;
-import org.pushingpixels.flamingo.api.common.popup.PopupPanelCallback;
 import org.pushingpixels.flamingo.api.ribbon.JFlowRibbonBand;
 import org.pushingpixels.flamingo.api.ribbon.resize.CoreRibbonResizePolicies;
 import org.pushingpixels.flamingo.api.ribbon.resize.IconRibbonBandResizePolicy;
@@ -74,73 +67,6 @@ public class ZoomContributorFactory implements IRibbonContributorFactory {
 				getAccelChangeListener().addAction(action.getKey(), button);
 				addDefaultToggleHandler(context, action, button);
 				band.addFlowComponent(button);
-				
-				action = context.getBuilder().getMode().getAction("CenterSelectedNodeAction");
-				button= RibbonActionContributorFactory.createCommandButton(action);
-				button.setDisplayState(CommandButtonDisplayState.MEDIUM);
-				button.setCommandButtonKind(CommandButtonKind.ACTION_AND_POPUP_MAIN_ACTION);
-				getAccelChangeListener().addAction(action.getKey(), button);
-				button.setPopupCallback(new PopupPanelCallback() {
-					public JPopupPanel getPopupPanel(JCommandButton commandButton) {
-						JCommandPopupMenu popupmenu = new JCommandPopupMenu();
-						SetBooleanPropertyAction booleanAction = (SetBooleanPropertyAction) context.getBuilder().getMode().getAction("SetBooleanPropertyAction.center_selected_node");
-						final JCommandToggleMenuButton toggleButton = RibbonActionContributorFactory.createCommandToggleMenuButton(booleanAction);						
-						toggleButton.getActionModel().setSelected(booleanAction.isPropertySet());
-						KeyStroke ks = context.getBuilder().getAcceleratorManager().getAccelerator(booleanAction.getKey());
-    					RibbonActionContributorFactory.updateRichTooltip(toggleButton, booleanAction, ks);
-						popupmenu.addMenuButton(toggleButton);
-						return popupmenu;
-					}
-				});
-				addDefaultToggleHandler(context, action, button);
-				band.addFlowComponent(button);
-				
-				button = new JCommandButton(TextUtils.getText("menu_viewmode"));
-				button.setDisplayState(CommandButtonDisplayState.MEDIUM);
-				button.setCommandButtonKind(CommandButtonKind.POPUP_ONLY);				
-				button.setPopupCallback(new PopupPanelCallback() {
-					public JPopupPanel getPopupPanel(JCommandButton commandButton) {
-    					JCommandPopupMenu popupmenu = new JCommandPopupMenu();
-    					
-    					AFreeplaneAction action = context.getBuilder().getMode().getAction("ViewLayoutTypeAction.OUTLINE");
-    					JCommandToggleMenuButton toggleButton = RibbonActionContributorFactory.createCommandToggleMenuButton(action);
-    					getAccelChangeListener().addAction(action.getKey(), toggleButton);
-    					KeyStroke ks = context.getBuilder().getAcceleratorManager().getAccelerator(action.getKey());
-    					RibbonActionContributorFactory.updateRichTooltip(toggleButton, action, ks);    					
-    					popupmenu.addMenuButton(toggleButton);
-    					
-    					action = context.getBuilder().getMode().getAction("ToggleFullScreenAction");
-    					toggleButton = RibbonActionContributorFactory.createCommandToggleMenuButton(action);
-    					ks = context.getBuilder().getAcceleratorManager().getAccelerator(action.getKey());
-    					RibbonActionContributorFactory.updateRichTooltip(toggleButton, action, ks);
-    					popupmenu.addMenuButton(toggleButton);
-    					
-    					SetBooleanPropertyAction presentationModeAction = (SetBooleanPropertyAction) context.getBuilder().getMode().getAction("SetBooleanPropertyAction.presentation_mode");    					
-    					toggleButton = RibbonActionContributorFactory.createCommandToggleMenuButton(presentationModeAction);
-    					toggleButton.getActionModel().setSelected(presentationModeAction.isPropertySet());
-    					ks = context.getBuilder().getAcceleratorManager().getAccelerator(presentationModeAction.getKey());
-    					RibbonActionContributorFactory.updateRichTooltip(toggleButton, presentationModeAction, ks);    					
-    					popupmenu.addMenuButton(toggleButton);
-    					
-    					action = context.getBuilder().getMode().getAction("ShowSelectionAsRectangleAction");
-    					toggleButton = RibbonActionContributorFactory.createCommandToggleMenuButton(action);
-    					ks = context.getBuilder().getAcceleratorManager().getAccelerator(action.getKey());
-    					RibbonActionContributorFactory.updateRichTooltip(toggleButton, action, ks);    					
-    					popupmenu.addMenuButton(toggleButton);
-    					
-    					SetBooleanPropertyAction highlightFormulasAction = (SetBooleanPropertyAction) context.getBuilder().getMode().getAction("SetBooleanPropertyAction.highlight_formulas");
-    					toggleButton = RibbonActionContributorFactory.createCommandToggleMenuButton(highlightFormulasAction);
-    					toggleButton.getActionModel().setSelected(highlightFormulasAction.isPropertySet());
-    					ks = context.getBuilder().getAcceleratorManager().getAccelerator(highlightFormulasAction.getKey());
-    					RibbonActionContributorFactory.updateRichTooltip(toggleButton, highlightFormulasAction, ks);    					
-    					popupmenu.addMenuButton(toggleButton);
-    					
-    					return popupmenu;
-    				}
-				});
-				addDefaultToggleHandler(context, action, button);
-				band.addFlowComponent(button);
-				
 				
 				List<RibbonBandResizePolicy> policies = new ArrayList<RibbonBandResizePolicy>();				
 				policies.add(new CoreRibbonResizePolicies.FlowThreeRows(band.getControlPanel()));
