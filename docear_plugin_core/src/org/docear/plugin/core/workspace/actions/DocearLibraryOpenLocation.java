4d1b35e3556f455b15acf520849ab4ed7e808e4d
Marcel Genzmehr <genzmehr@docear.org>
2013-08-05 15:08:28 +0200

    fixed "open location" action enabled method

--- docear_plugin_core/src/org/docear/plugin/core/workspace/actions/DocearLibraryOpenLocation.java
+++ docear_plugin_core/src/org/docear/plugin/core/workspace/actions/DocearLibraryOpenLocation.java
@@ -2,13 +2,22 @@ package org.docear.plugin.core.workspace.actions;
 
 import java.awt.event.ActionEvent;
 
+import java.io.File;
+
+import javax.swing.tree.TreePath;
+
 import org.docear.plugin.core.workspace.model.DocearWorkspaceProject;
 import org.docear.plugin.core.workspace.node.FolderTypeLibraryNode;
+import org.freeplane.core.ui.EnabledAction;
 import org.freeplane.plugin.workspace.URIUtils;
+import org.freeplane.plugin.workspace.WorkspaceController;
 import org.freeplane.plugin.workspace.actions.NodeOpenLocationAction;
+import org.freeplane.plugin.workspace.components.menu.CheckEnableOnPopup;
 import org.freeplane.plugin.workspace.model.AWorkspaceTreeNode;
 import org.freeplane.plugin.workspace.model.project.AWorkspaceProject;
 
+@CheckEnableOnPopup
+@EnabledAction(checkOnNodeChange=true)
 public class DocearLibraryOpenLocation extends NodeOpenLocationAction {
 	
 	private static final long serialVersionUID = 1L;
@@ -17,6 +26,18 @@ public class DocearLibraryOpenLocation extends NodeOpenLocationAction {
 		super();
 	}
 	
+	@Override
+	public void setEnabledFor(AWorkspaceTreeNode node, TreePath[] selectedPaths) {
+		if(node != null && node instanceof FolderTypeLibraryNode) {
+			AWorkspaceProject project = WorkspaceController.getSelectedProject(node);
+			File f = URIUtils.getAbsoluteFile(((DocearWorkspaceProject)project).getProjectLibraryPath());
+			setEnabled(f.exists());
+		}
+		else {
+			super.setEnabledFor(node, selectedPaths);
+		}
+	}
+
 	public void actionPerformed(ActionEvent event) {
 		AWorkspaceTreeNode targetNode = getNodeFromActionEvent(event);
 		if(targetNode != null && targetNode instanceof FolderTypeLibraryNode) {
