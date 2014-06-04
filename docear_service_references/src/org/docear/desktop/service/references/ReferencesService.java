bb3c61c148b881f22237f6061d98f0a0d36e13ab
Marcel Genzmehr <genzmehr@docear.org>
2014-06-04 17:24:44 +0200

    added another docear service project and some refactoring

---
+++ docear_service_references/src/org/docear/desktop/service/references/ReferencesService.java
@@ -0,0 +1,21 @@
+package org.docear.desktop.service.references;
+
+import org.freeplane.plugin.docear.core.DocearServiceContext;
+import org.freeplane.plugin.docear.core.spi.DocearService;
+
+public class ReferencesService implements DocearService {
+
+	protected ReferencesService() {
+	}
+	
+	public void start(DocearServiceContext context) {
+		System.out.println("References Service starting ...");
+		
+	}
+
+	public void stop(DocearServiceContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
