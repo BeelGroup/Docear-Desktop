b18201219d70dffee4e109a6e2a368b08cd77c07
Stefan Langer <langer@docear.org>
2014-08-29 11:25:48 +0200

    use MultiLineActionLabel for dialog (#405)

--- docear_plugin_pdfutilities/src/org/docear/plugin/pdfutilities/ui/ReadOnlyDocumentExceptionPage.java
+++ docear_plugin_pdfutilities/src/org/docear/plugin/pdfutilities/ui/ReadOnlyDocumentExceptionPage.java
@@ -1,14 +1,18 @@
 package org.docear.plugin.pdfutilities.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 import org.docear.plugin.core.ui.MultiLineActionLabel;
 import org.docear.plugin.core.ui.wizard.AWizardPage;
 import org.docear.plugin.core.ui.wizard.WizardSession;
+import org.docear.plugin.pdfutilities.PdfUtilitiesController;
 import org.freeplane.core.util.TextUtils;
 
 import de.intarsys.tools.locator.FileLocator;
-import java.awt.BorderLayout;
 
 public class ReadOnlyDocumentExceptionPage extends AWizardPage {
 	
@@ -24,8 +28,7 @@ public class ReadOnlyDocumentExceptionPage extends AWizardPage {
 		setLayout(new BorderLayout());
 		
 		textLabel = new MultiLineActionLabel();
-		textLabel.setBackground(Color.WHITE);
-		textLabel.setText(TextUtils.format("docear.pdf.readonly.warning.text", ""));
+		textLabel.setBackground(Color.WHITE);		
 		add(textLabel, BorderLayout.CENTER);
 	}
 	/***********************************************************************************
@@ -44,9 +47,19 @@ public class ReadOnlyDocumentExceptionPage extends AWizardPage {
 	@Override
 	public void preparePage(WizardSession session) {
 		try {
-			String file = session.get(FileLocator.class).getCanonicalFile().getName();
-			textLabel.setText(TextUtils.format("docear.pdf.readonly.warning.text", file));
-		}catch (Exception e) {
+			final String file = session.get(FileLocator.class).getCanonicalFile().getAbsolutePath();
+			String text = TextUtils.format("docear.pdf.readonly.warning.text", file);
+			System.out.println("text: "+text);
+			textLabel.setText(text);
+			textLabel.addActionListener(new ActionListener() {				
+				public void actionPerformed(ActionEvent e) {
+					if (e.getActionCommand().equals("open_file")) {
+						PdfUtilitiesController.getController().openPdfOnPage(new File(file).toURI(), 1);
+					}
+				}
+			});
+		}
+		catch (Exception e) {
 			textLabel.setText(TextUtils.format("docear.pdf.readonly.warning.text", ""));
 		}
 		session.setWizardTitle(TextUtils.getText("docear.pdf.readonly.warning.title"));
